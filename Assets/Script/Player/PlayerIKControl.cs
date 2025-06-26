831c77418d96591d92cce1b8c6a9d5ecc8ea185e
opoi <10419317+opoinbt@user.noreply.gitee.com>
2025-06-26 13:34:13 +0800

    feat:添加了基础动画库，新增ik控制脚本

---
+++ Assets/Script/Player/PlayerIKControl.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+
+namespace Script.Player
+{
+    public class PlayerIKControl:MonoBehaviour
+    {
+        [SerializeField]
+        private Animator animator;
+        [SerializeField]
+        private Transform leftHand;
+        [SerializeField]
+        private Transform rightHand;
+
+        private void Awake()
+        {
+            Init();
+        }
+        
+        public void Init()
+        {
+            if (!animator)
+            {
+                animator = GetComponent<Animator>();
+            }
+
+            if (!leftHand)
+            {
+                leftHand = GameObject.Find("L_Hand_IK").transform;
+            }
+
+            if (!rightHand)
+            {
+                rightHand = GameObject.Find("R_Hand_IK").transform;
+            }
+        }
+
+        private void OnAnimatorIK(int layerIndex)
+        {
+            //设置IK位置
+            IKControl(AvatarIKGoal.LeftHand, leftHand);
+            IKControl(AvatarIKGoal.RightHand, rightHand);
+            
+        }
+
+        private void IKControl(AvatarIKGoal goal,Transform target)
+        {
+            animator.SetIKPositionWeight(goal, 1.0f);
+            animator.SetIKPosition(goal, target.position);
+        }
+        private void OnDestroy()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            animator = null;
+            leftHand = null;
+            rightHand = null;
+        }
+    }
+}
